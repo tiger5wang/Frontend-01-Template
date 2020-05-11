9cf8966798f6c3e1c000849ab45723b3437f0e9a
wangyuhu <826969978@qq.com>
2020-05-11 22:19:20 +0800

    Request

--- week05/client.js
+++ week05/client.js
@@ -1,14 +1,78 @@
 const net = require('net');
 
-const client = net.createConnection({host: "127.0.0.1" ,port: 8088 }, () => {
+class Request {
+    // method,url = host + port + path;
+    // headers
+    // body: k/v
+    constructor(options) {
+        this.menthod = options.method || 'GET   ';
+        this.host = options.host;
+        this.port = options.port || 80;
+        this.path = options.path || '/';
+        this.body = options.body || {};
+        this.headers = options.headers || {};
+        // 设置默认 Content-Type
+        if(!this.headers['Content-Type']) {
+            this.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        }
+        // 先用下面两种 Content-Type
+        if(this.headers['Content-Type'] === 'application/json') {
+            this.bodyText = JSON.stringify(this.body);
+        } else if(this.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+            this.bodyText = Object.keys(this.body).map(key => `${key}=${this.body[key]}`).join('&')
+        }
+        this.headers['Content-Length'] = this.bodyText.length;
+    }
+
+    toString() {
+        return `${this.menthod}${this.path} HTTP/1.1\r
+${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n
+${this.bodyText}
+        `
+    }
+
+    send(connection) {
+        return new Promise((resolve, reject) => {
+            if(connection) {
+                connection.write(this.toString())
+            } else {
+                connection = net.createConnection({
+                    host: this.host,
+                    port: this.port
+                }, () => {
+                    connection.write(this.toString())
+                })
+            }
+            connection.on('data', (data) => {
+                resolve(data.toString());
+                connection.end();
+                });
+
+            connection.on('error', (data) => {
+                reject(data.toString());
+                connection.end();
+            });
+        })
+    }
+
+}
+
+// 一、实现 send() 方法之前
+/* const client = net.createConnection({host: "127.0.0.1" ,port: 8088 }, () => {
     // 'connect' listener.
     console.log('connected to server!');
-    // client.write('POST / HTTP/1.1\r\n');
-    // client.write('Host: 127.0.0.1\r\n');
-    // client.write('Content-Length: 20\r\n');
-    // client.write('Content-Type: application/x-www-form-urlencoded\r\n');
-    // client.write('\r\n');
-    // client.write('field1=aaa&code=x%3D')
+
+    // 请求行
+    client.write('POST / HTTP/1.1\r\n');  // 注意要添加后面的 \r\n 换行
+    // 请求头
+    client.write('Host: 127.0.0.1\r\n');  // 请求头的每一项同样需要 注意要添加后面的 \r\n 换行
+    client.write('Content-Length: 20\r\n');   // 当请求体有数据的时候， Content-Length 不能省略，并且字符数要对应请求体的字符数，否则都会 Bad Request。
+    client.write('Content-Type: application/x-www-form-urlencoded\r\n');
+    // 空行， 这个空行一定不能省略，它是区分请求头和请求体的标志。
+    client.write('\r\n');
+    // 下面为请求体
+    client.write('field1=aaa&code=x%3D') 
+    // 上面几行为 实验的一个例子
 
     let request = new Request({
         menthod: 'POST',
@@ -22,6 +86,7 @@ const client = net.createConnection({host: "127.0.0.1" ,port: 8088 }, () => {
             name: 'tiget'
         }
     })
+
     console.log(request.toString())
     client.write(request.toString())
   });
@@ -32,62 +97,27 @@ const client = net.createConnection({host: "127.0.0.1" ,port: 8088 }, () => {
   });
   client.on('end', () => {
     console.log('disconnected from server');
-  });
+  }); */
 
-void 
-
-class Request {
-// method,url = host + port + path;
-// body: k/v
-// Headers
-    constructor(options) {
-        this.menthod = options.method || 'GET   ';
-        this.host = options.host;
-        this.port = options.port || 80;
-        this.path = options.path || '/';
-        this.body = options.body || {};
-        this.headers = options.headers || {};
-        if(!this.headers['Content-Type']) {
-            this.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+// 二、实现send() 方法后
+void async function() {
+    let request = new Request({
+        menthod: 'POST',
+        host: '127.0.0.1',
+        port: 8088,
+        path: '/',
+        headers: {
+            'x-foo2': 'haha'
+        },
+        body: {
+            name: 'tiget'
         }
+    });
+    let response  = await request.send();
+    console.log(response)
+}()
 
-        if(this.headers['Content-Type'] === 'application/json') {
-            this.bodyText = JSON.stringify(this.body);
-        } else if(this.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
-            this.bodyText = Object.keys(this.body).map(key => `${key}=${this.body[key]}`)
-        }
-        this.headers['Content-Length'] = this.bodyText.length;
-    }
 
-    toString() {
-        return `${this.menthod} ${this.path} HTTP/1.1\r\n
-        ${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`)}
-        `
-    }
-
-    send(connection) {
-        if(connection) {
-            connection.write(this.toString())
-        } else {
-            connection = net.createConnection({
-                host: this.host,
-                port: this.port
-            }, () => {
-                connection.write(this.toString())
-            })
-            connection.on('data', (data) => {
-                resolve(data.toString());
-                connection.end();
-              });
-
-              connection.on('error', (data) => {
-            reject(data.toString());
-            connection.end();
-            });
-        }
-    }
-
-}
 
 class Response {
 
