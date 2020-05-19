832569c5bcb8655b7a56fdf6a2c15268c2e160a4
wangyuhu <826969978@qq.com>
2020-05-19 11:16:57 +0800

    parserTag

--- week06/parser.js
+++ week06/parser.js
@@ -1,24 +1,32 @@
 const EOF = Symbol('EOF');  // EPF: end of file;
 
+
+
+// 元素的内容部分
 function data(char) {
-    if (char === '<')
-        return tagOpen;
-    else if (char === EOF)
+    if (char === '<') {
+		console.log(currentToken);
+		return statTagOpen;
+    } else if (char === EOF)
         return ;
     else
         return data;
 }
 
-function tagOpen(char) {
-    if (char === '/')
-        return tagClose;
-    else if (char.match(/^[a-zA-Z]$/))
-        return tagName(char);
-    else
+// 元素的开始标签
+function statTagOpen(char) {
+    if(char === '/') {  // statTagOpen 状态如果是 / 那就表明这个标签是结束标签，切换状态
+        return endTagOpen;
+    } else if(char.match(/^[a-zA-Z]$/)) {  // 如果碰到正常字符 a-zA-Z， 表明开始标签名部分了
+		return tagName(char);
+    } else
         return data;
 }
-
-function tagClose(char) {
+// 元素的结束标签
+function endTagOpen(char) {
+    if(char.match(/^[a-zA-Z]$/)) {
+        return tagName(char);
+    }
     if (char === '>') {
         return data;
     } else if (char === EOF) {
@@ -29,21 +37,46 @@ function tagClose(char) {
 }
 
 function tagName(char) {
-
+    if(char.match(/^[a-zA-Z]$/)) {
+        return tagName;
+    } else if(char.match(/^[\t\n\ ]&/)) {
+        return beforeAttributeName;
+    } else if(char === '/') {
+        return selfClosureTag;
+    } else if(char === '>') {
+        return data;
+    } else {
+        return tagName;
+    }
 }
 
 function beforeAttributeName(char) {
-
+    if(char.match(/^[\t\n\f ]$/)) {  // 标签名和属性之间 空格可能有多个
+        return beforeAttributeName;
+    } else if(char === ">") {
+        return data;
+    } else if(char === '=') {
+        return beforeAttributeName;
+    } else {
+        return beforeAttributeName;
+    }
 }
 
 function selfClosureTag(char) {
-
+    if(char === '>') {
+        currentToken.isSelfClosure = true;
+        return data;
+    } else if(char === 'EOF') {
+    
+    } else {
+    
+    }
 }
 
-module.exports.parseHTML = function (html) {
+module.exports.parserHTML = function (html) {
     let state = data;
     for (let char of html) {
         state = state(char)
     }
     return state(EOF)
-}
+};
