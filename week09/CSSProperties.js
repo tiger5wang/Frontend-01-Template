9aecb393f30a12e3aae9b955ee3f40b14080eb8b
wangyuhu <826969978@qq.com>
2020-06-10 18:06:17 +0800

    css property

--- week09/CSSProperties.js
+++ week09/CSSProperties.js
@@ -231,4 +231,173 @@ let properties = [
 	"line-break",
 ];
 
+CSS  Inline  Layout  Module  Level 3 |
+dominant-baseline, vertical-align, baseline-source, alignment-baseline, baseline-shift, line-height, line-sizing, leading-trim-over, leading-trim-under, leading-trim, inline-sizing, initial-letter, initial-letter-align, initial-letter-wrap
 
+CSS  Containment  Module  Level 2 |
+contain, content-visibility
+
+CSS  Overflow  Module  Level 3 |
+overflow-x, overflow-y, overflow, overflow-clip-margin, overflow-inline, overflow-block, text-overflow, block-ellipsis, line-clamp, max-lines, continue
+
+CSS  Box  Sizing  Module  Level 4 |
+aspect-ratio, contain-intrinsic-size
+
+CSS  Positioned  Layout  Module  Level 3 |
+position, top, right, bottom, left, inset-block-start, inset-inline-start, inset-block-end, inset-inline-end, inset-block, inset-inline, inset
+
+CSS  Display  Module  Level 3 | display
+
+CSS  Text  Decoration  Module  Level 4 |
+text-decoration-line, text-decoration-style, text-decoration-color, text-decoration-thickness, text-decoration, text-underline-position, text-underline-offset, text-decoration-skip, text-decoration-skip-self, text-decoration-skip-box, text-decoration-skip-inset, text-decoration-skip-spaces, text-decoration-skip-ink, text-emphasis-style, text-emphasis-color, text-emphasis, text-emphasis-position, text-emphasis-skip, text-shadow
+
+CSS  Ruby  Layout  Module  Level 1 |
+ruby-position, ruby-merge, ruby-align, ruby-overhang
+
+CSS  Text  Module  Level 3 |
+text-transform, white-space, tab-size, word-break, line-break, hyphens, overflow-wrap, word-wrap, text-align, text-align-all, text-align-last, text-justify, word-spacing, letter-spacing, text-indent, hanging-punctuation
+
+CSS  Box  Model  Module  Level 3 |
+margin-top, margin-right, margin-bottom, margin-left, margin, padding-top, padding-right, padding-bottom, padding-left, padding
+
+CSS  Box  Model  Module  Level 4 |
+margin-top, margin-right, margin-bottom, margin-left, margin, margin-trim, padding-top, padding-right, padding-bottom, padding-left, padding
+
+CSS  Box  Alignment  Module  Level 3 |
+align-content, justify-content, place-content, justify-self, align-self, place-self, justify-items, align-items, place-items, row-gap, column-gap, gap
+
+CSS  Color  Adjustment  Module  Level 1 |
+color-scheme, forced-color-adjust, color-adjust
+
+CSS  Transforms  Module  Level 2 |
+translate, rotate, scale, transform-style, perspective, perspective-origin, backface-visibility
+
+CSS  Scroll  Anchoring  Module  Level 1 |
+overflow-anchor
+
+CSS  Basic  User  Interface  Module  Level 4 |
+outline, outline-width, outline-style, outline-color, outline-offset, resize, cursor, caret-color, caret-shape, caret, nav-up, nav-right, nav-down, nav-left, user-select, appearance
+
+CSS  Writing  Modes  Level 3 |
+direction, unicode-bidi, writing-mode, text-orientation, glyph-orientation-vertical, text-combine-upright
+
+CSS  Spatial  Navigation  Level 1 |
+spatial-navigation-contain, spatial-navigation-action, spatial-navigation-function
+	CSS  Containment  Module  Level 1 | contain
+
+CSS  Text  Module  Level 4 |
+word-boundary-detection, word-boundary-expansion, text-space-collapse, text-space-trim, text-wrap, wrap-before, wrap-after, wrap-inside, white-space, hyphenate-character, hyphenate-limit-zone, hyphenate-limit-chars, hyphenate-limit-lines, hyphenate-limit-last, text-group-align, line-padding, text-spacing
+
+CSS  Fonts  Module  Level 4 |
+font-family, font-weight, font-stretch, font-style, font-size, font-size-adjust, font, font-synthesis-weight, font-synthesis-style, font-synthesis-small-caps, font-synthesis, font-kerning, font-variant-ligatures, font-variant-position, font-variant-caps, font-variant-numeric, font-variant-alternates, font-variant-east-asian, font-variant, font-feature-settings, font-language-override, font-optical-sizing, font-variation-settings, font-palette, font-variant-emoji
+
+CSS  Color  Module  Level 4 |
+color, opacity
+
+CSS  Multi-column  Layout  Module  Level 1 |
+column-width, column-count, columns, column-rule-color, column-rule-style, column-rule-width, column-rule, column-span, column-fill
+
+CSS  Images  Module  Level 3 |
+object-fit, object-position, image-orientation, image-rendering
+
+CSS  Lists  Module  Level 3 |
+list-style-image, list-style-type, list-style-position, list-style, marker-side, counter-reset, counter-increment, counter-set
+
+CSS  Text  Decoration  Module  Level 3 |
+text-decoration-line, text-decoration-style, text-decoration-color, text-decoration, text-underline-position, text-emphasis-style, text-emphasis-color, text-emphasis, text-emphasis-position, text-shadow
+
+CSS  Generated  Content  Module  Level 3 |
+content, quotes, string-set, bookmark-level, bookmark-label, bookmark-state
+
+CSS  Writing  Modes  Level 4 |
+direction, unicode-bidi, writing-mode, text-orientation, glyph-orientation-vertical, text-combine-upright
+
+CSS  Table  Module  Level 3 |
+table-layout, border-collapse, border-spacing, caption-side, empty-cells
+
+CSS  Overscroll  Behavior  Module  Level 1 |
+overscroll-behavior, overscroll-behavior-x, overscroll-behavior-y, overscroll-behavior-inline, overscroll-behavior-block
+
+CSS  Intrinsic  &  Extrinsic  Sizing  Module  Level 3 |
+width, height, min-width, min-height, max-width, max-height, box-sizing
+
+CSS  Scroll  Snap  Module  Level 1 |
+scroll-snap-type, scroll-padding, scroll-margin, scroll-snap-align, scroll-snap-stop, scroll-padding-top, scroll-padding-right, scroll-padding-bottom, scroll-padding-left, scroll-padding-inline-start, scroll-padding-block-start, scroll-padding-inline-end, scroll-padding-block-end, scroll-padding-block, scroll-padding-inline, scroll-margin-top, scroll-margin-right, scroll-margin-bottom, scroll-margin-left, scroll-margin-block-start, scroll-margin-inline-start, scroll-margin-block-end, scroll-margin-inline-end, scroll-margin-block, scroll-margin-inline
+
+CSS  Transforms  Module  Level 1 |
+transform, transform-origin, transform-box
+Motion  Path  Module  Level 1 | offset-path, offset-distance, offset-position, offset-anchor, offset-rotate, offset
+Filter  Effects  Module  Level 1 | filter, flood-color, flood-opacity, color-interpolation-filters, lighting-color
+
+CSS  Fragmentation  Module  Level 4 |
+break-before, break-after, break-inside, orphans, widows, margin-break, box-decoration-break
+
+CSS  Fragmentation  Module  Level 3 |
+break-before, break-after, break-inside, orphans, widows, box-decoration-break
+
+CSS  Flexible  Box  Layout  Module  Level 1 |
+flex-direction, flex-wrap, flex-flow, order, flex, flex-grow, flex-shrink, flex-basis, justify-content, align-items, align-self, align-content
+
+CSS  Paged  Media  Module  Level 3 | page
+
+CSS  Animations  Level 1 |
+animation-name, animation-duration, animation-timing-function, animation-iteration-count, animation-direction, animation-play-state, animation-delay, animation-fill-mode, animation
+
+CSS Transitions |
+transition-property, transition-duration, transition-timing-function, transition-delay, transition
+
+CSS  Scrollbars  Module  Level 1 | scrollbar-color, scrollbar-width
+
+CSS  Cascading  and  Inheritance  Level 4 | all
+
+CSS  Cascading  and  Inheritance  Level 3 | all
+
+CSS  Logical  Properties  and  Values  Level 1 |
+block-size, inline-size, min-block-size, min-inline-size, max-block-size, max-inline-size, margin-block-start, margin-block-end, margin-inline-start, margin-inline-end, margin-block, margin-inline, inset-block-start, inset-block-end, inset-inline-start, inset-inline-end, inset-block, inset-inline, inset, padding-block-start, padding-block-end, padding-inline-start, padding-inline-end, padding-block, padding-inline, border-block-start-width, border-block-end-width, border-inline-start-width, border-inline-end-width, border-block-width, border-inline-width, border-block-start-style, border-block-end-style, border-inline-start-style, border-inline-end-style, border-block-style, border-inline-style, border-block-start-color, border-block-end-color, border-inline-start-color, border-inline-end-color, border-block-color, border-inline-color, border-block-start, border-block-end, border-inline-start, border-inline-end, border-block, border-inline, border-start-start-radius, border-start-end-radius, border-end-start-radius, border-end-end-radius
+
+CSS Basic User Interface Module Level 3 (CSS3 UI) |
+box-sizing, outline, outline-width, outline-style, outline-color, outline-offset, resize, text-overflow, cursor, caret-color
+
+CSS  Grid  Layout  Module  Level 1 |
+grid-template-columns, grid-template-rows, grid-template-areas, grid-template, grid-auto-columns, grid-auto-rows, grid-auto-flow, grid, grid-row-start, grid-column-start, grid-row-end, grid-column-end, grid-row, grid-column, grid-area
+
+CSS  Backgrounds  and  Borders  Module  Level 3 |
+background-color, background-image, background-repeat, background-attachment, background-position, background-clip, background-origin, background-size, background, border-top-color, border-right-color, border-bottom-color, border-left-color, border-color, border-top-style, border-right-style, border-bottom-style, border-left-style, border-style, border-top-width, border-right-width, border-bottom-width, border-left-width, border-width, border-top, border-right, border-bottom, border-left, border, border-top-left-radius, border-top-right-radius, border-bottom-right-radius, border-bottom-left-radius, border-radius, border-image-source, border-image-slice, border-image-width, border-image-outset, border-image-repeat, border-image, box-shadow
+
+CSS  Overflow  Module  Level 4 | scrollbar-gutter, continue, max-lines
+
+CSS  Fill  and  Stroke  Module  Level 3 |
+fill-rule, fill-break, fill-color, fill-image, fill-origin, fill-position, fill-size, fill-repeat, fill, fill-opacity, stroke-width, stroke-align, stroke-linecap, stroke-linejoin, stroke-miterlimit, stroke-break, stroke-dasharray, stroke-dashoffset, stroke-dash-corner, stroke-dash-justify, stroke-color, stroke-image, stroke-origin, stroke-position, stroke-size, stroke-repeat, stroke, stroke-opacity
+
+CSS  Image  Values  and  Replaced  Content  Module  Level 4 | image-resolution
+
+CSS Rhythmic Sizing |
+line-height-step, block-step-size, block-step-insert, block-step-align, block-step-round, block-step
+
+CSS  Round  Display  Level 1 | shape-inside, border-boundary
+CSSOM View Module | scroll-behavior
+
+CSS  Custom  Properties  for  Cascading  Variables  Module  Level 1 | --*
+
+CSS  Will  Change  Module  Level 1 | will-change
+
+CSS Page Floats |
+float-reference, float, clear, float-defer, float-offset
+Priorities for CSS from the Digital Publishing Interest Group | hyphenate-patterns, min-length-of-last-line, merge-sequential-page-numbers
+
+CSS  Exclusions  Module  Level 1 |
+wrap-flow, wrap-through
+Compositing  and  Blending  Level 1 | mix-blend-mode, isolation, background-blend-mode
+
+CSS  Regions  Module  Level 1 |
+flow-into, flow-from, break-before, break-after, break-inside, region-fragment
+
+CSS  Line  Grid  Module  Level 1 | line-grid, line-snap, box-snap
+
+CSS  Masking  Module  Level 1 |
+clip-path, clip-rule, mask-image, mask-mode, mask-repeat, mask-position, mask-clip, mask-origin, mask-size, mask-composite, mask, mask-border-source, mask-border-mode, mask-border-slice, mask-border-width, mask-border-outset, mask-border-repeat, mask-border, mask-type, clip
+
+CSS Generated Content for Paged Media Module |
+string-set, running, footnote-display, footnote-policy, bookmark-level, bookmark-label, bookmark-state, running
+
+CSS  Shapes  Module  Level 1 | shape-outside, shape-image-threshold, shape-margin
