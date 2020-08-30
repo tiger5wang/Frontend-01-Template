9f305339d7f1176dabe1d5cb3b8791041eebb9a3
wangyuhu <826969978@qq.com>
2020-08-30 15:57:01 +0800

    githooks

---
+++ week21/githooks/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+        // enable additional rules
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+
+        // override default options for rules from base configurations
+        "comma-dangle": ["error", "always"],
+        "no-cond-assign": ["error", "always"],
+
+        // disable rules from base configurations
+        "no-console": "off",
+    }
+};
---
+++ week21/githooks/main.js
@@ -0,0 +1,7 @@
+let a = 2;
+
+if (a === 3) {
+    let b = 3;
+    b = 2;
+    b.toString()
+}
